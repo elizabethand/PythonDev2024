5aa245b916a0180b1b7502be5398bf8ffff7a0ae
Andreeva Elizaveta <elizabeth01@bk.ru>
2024-03-04 18:52:37 +0300

    Add 03_MergeRequirements/bullscows.py

---
+++ 03_MergeRequirements/bullscows.py
@@ -0,0 +1,15 @@
+def bullscows(guess: str, secret: str) -> tuple[int, int]:
+    pass
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    pass
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    pass
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    print("Run bullscows.py ...")
