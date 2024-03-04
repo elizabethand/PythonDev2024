6dc2214a8fa31c2ed147e166993188201efed93d
Andreeva Elizaveta <elizabeth01@bk.ru>
2024-03-04 18:59:17 +0300

    Add gameplay implementation

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,8 +1,20 @@
+import random
+
+
 def bullscows(guess: str, secret: str) -> tuple[int, int]:
     pass
 
 def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
-    pass
+    secret_word = random.choice(words)
+    attempts = 1
+    while True:
+        guess = ask("Введите слово: ", words)
+        bulls, cows = bullscows(guess, secret_word)
+        inform("Быки: {}, Коровы: {}", bulls, cows)
+        attempts += 1
+        if bulls == len(secret_word):
+            return attempts
+
 
 def ask(prompt: str, valid: list[str] = None) -> str:
     pass
