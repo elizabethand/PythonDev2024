e679bf19dc82f48122f677eb7a246942f996b5d3
Andreeva Elizaveta <elizabeth01@bk.ru>
2024-03-04 19:03:49 +0300

    Add inform implementation

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -24,7 +24,7 @@ def ask(prompt: str, valid: list[str] = None) -> str:
     return user_input
 
 def inform(format_string: str, bulls: int, cows: int) -> None:
-    pass
+    print(format_string.format(bulls, cows))
 
 
 if __name__ == "__main__":
